0eaea816115ffa9400b27f6f8d1a0740db10b552
mohamedaty20 <86588628+mohamedaty20@users.noreply.github.com>
2026-09-11 17:18:44 +0300

    Create rates.py

---
+++ services/oq/rates.py
@@ -0,0 +1,10 @@
+"""
+services/boq/rates.py
+
+Deprecated. Prices were removed from the engine.
+This file exists only so old imports don't break.
+"""
+
+DEFAULT_RATES = {}
+RATE_LABELS = {}
+RATE_UNITS = {}
